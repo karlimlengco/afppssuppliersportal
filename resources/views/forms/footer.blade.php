1b235bbb539813a771804a15615fa7a1604b5f39
Karl Limlengco <kslimlengco@revlv.net>
2017-07-20 09:17:34 +0800

    udate

--- resources/views/forms/footer.blade.php
+++ resources/views/forms/footer.blade.php
@@ -4,22 +4,26 @@
     <link rel="stylesheet" href="{{base_path('public/css/main.css')}}">
 </head>
 <body>
-<div class="printable-form__foot">
-    <table class="printable-form__foot__table">
-        <tr>
-            <td colspan="2">
-                <p class="printable-form__foot__values">AFP Core Values: Honor, Service, Patriotism</p>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <span class="printable-form__foot__ref">11111 1111 11111</span>
-            </td>
-            <td>
-                <span class="printable-form__foot__code"><img src="{{base_path('public/img/barcode.png')}}" alt=""></span>
-            </td>
-        </tr>
-    </table>
+<div class="printable-form-wrapper">
+    <div class="printable-form">
+        <div class="printable-form__foot">
+            <table class="printable-form__foot__table">
+                <tr>
+                    <td colspan="2">
+                        <p class="printable-form__foot__values">AFP Core Values: Honor, Service, Patriotism</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="printable-form__foot__ref">11111 1111 11111</span>
+                    </td>
+                    <td>
+                        <span class="printable-form__foot__code"><img src="{{base_path('public/img/barcode.png')}}" alt=""></span>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </div>
 </body>
 </html>
