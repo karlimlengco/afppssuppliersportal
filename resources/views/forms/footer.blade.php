583c05cbb74d726dd6596f135b994a5aa6f8529b
Karl Limlengco <kslimlengco@revlv.net>
2017-07-20 09:27:49 +0800

    udate

--- resources/views/forms/footer.blade.php
+++ resources/views/forms/footer.blade.php
@@ -3,48 +3,10 @@
 <head>
 <style type="text/css">
 
-.printable-form__foot {
-  position: absolute;
-  bottom: 10px;
-  left: 0;
-  right: 0;
-  width: 100%;
-}
-.printable-form__foot__table {
-  width: 100%;
-  border-collapse: collapse;
-}
-.printable-form__foot__table tr td {
-  width: 50%;
-  padding: 0 15px;
-}
-.printable-form__foot__values {
-  display: block;
-  width: 100%;
-  text-align: center;
-  padding: 0;
-  margin: 0 0 20px 0;
-}
-.printable-form__foot__ref {
-  display: block;
-  width: 100%;
-  font-size: 8px;
-  font-weight: 800;
-  text-transform: uppercase;
-  text-align: left;
-}
-.printable-form__foot__code {
-  display: block;
-  height: 20px;
-  text-align: right;
-}
-.printable-form__foot__code img {
-  height: 100%;
-}
 </style>
 </head>
 <body>
-<script>
+{{-- <script>
     function subst() {
         var vars={};
         var x=document.location.search.substring(1).split('&');
@@ -55,37 +17,34 @@
             for (var j=0; j<y.length; ++j) y[j].textContent = vars[x[i]];
         }
     }
-</script>
-<table style="width: 100%; border:0; margin: 0;" onload="subst()">
+</script> --}}
+{{-- <table style="width: 100%; border:0; margin: 0;" onload="subst()">
     <tr>
         <td style="background: #7D9CA4; color: #FFFFFF; float: left; padding: 10px; font-weight: bold; text-align: center;">
-            <span class="page"></span> of <span class="topage"></span>
+            <span class="page" style="color:black"></span> of <span style="color:black" class="topage"></span>
         </td>
         <td style="color: #000000; float: left; font-weight: bold; padding-top: 10px; text-align: center; width: 85%;">
             Text block
         </td>
     </tr>
 </table>
-<div class="printable-form-wrapper">
-    <div class="printable-form">
-        <div class="printable-form__foot">
-            <table class="printable-form__foot__table">
-                <tr>
-                    <td colspan="2">
-                        <p class="printable-form__foot__values">AFP Core Values: Honor, Service, Patriotism</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <span class="printable-form__foot__ref">11111 1111 11111</span>
-                    </td>
-                    <td>
-                        <span class="printable-form__foot__code"><img src="{{base_path('public/img/barcode.png')}}" alt=""></span>
-                    </td>
-                </tr>
-            </table>
-        </div>
-    </div>
+ --}}
+<div class="printable-form__foot" style="position: absolute; bottom: 10px;  left: 0; right: 0; width: 100%;font-size: 12px; font-weight: 400; color: #545454;">
+    <table class="printable-form__foot__table" style="width: 100%; border-collapse: collapse;">
+        <tr>
+            <td colspan="2" style="width: 50%; padding: 0 15px;">
+                <p class="printable-form__foot__values" style="display: block;  width: 100%; text-align: center; padding: 0; margin: 0 0 20px 0;">AFP Core Values: Honor, Service, Patriotism</p>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 50%; padding: 0 15px;">
+                <span class="printable-form__foot__ref" style="display: block; width: 100%; font-size: 8px; font-weight: 800; text-transform: uppercase; text-align: left;">11111 1111 11111</span>
+            </td>
+            <td style="width: 50%; padding: 0 15px;">
+                <span class="printable-form__foot__code" style="display: block; height: 20px; text-align: right;"><img style="height: 100%;" src="{{base_path('public/img/barcode.png')}}" alt=""></span>
+            </td>
+        </tr>
+    </table>
 </div>
 </body>
 </html>
