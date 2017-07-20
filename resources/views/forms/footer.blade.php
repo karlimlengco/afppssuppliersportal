290c4375e62f8db68169c141a2c2516455fd4d6b
Karl Limlengco <kslimlengco@revlv.net>
2017-07-20 09:36:21 +0800

    udate

--- resources/views/forms/footer.blade.php
+++ resources/views/forms/footer.blade.php
@@ -3,14 +3,6 @@
 <head>
 <style type="text/css">
 
-.printable-form__foot__code {
-  display: block;
-  height: 20px;
-  text-align: right;
-}
-.printable-form__foot__code img {
-  height: 100%;
-}
 </style>
 </head>
 <body>
@@ -26,7 +18,7 @@
                 <span class="printable-form__foot__ref" style="display: block; width: 100%; font-size: 8px; font-weight: 800; text-transform: uppercase; text-align: left;">11111 1111 11111</span>
             </td>
             <td >
-                <span class="printable-form__foot__code"  ><img  src="{{base_path('public/img/barcode.png')}}" alt=""></span>
+                <span><img  src="{{base_path('public/img/barcode.png')}}" alt=""></span>
             </td>
         </tr>
     </table>
