9445295669a88719b28b1574a03fd8449fdee9f0
Karl Limlengco <kslimlengco@revlv.net>
2017-08-15 21:49:39 +0800

    update rfq

--- resources/views/forms/rfq.blade.php
+++ resources/views/forms/rfq.blade.php
@@ -17,6 +17,7 @@
                 <div class="printable-form">
                     <!-- for dev reference -->
                     <!-- form header -->
+
                     <div class="printable-form__head">
                         <p class="printable-form__head__vision">AFP Vision 2028: A World-class Armed Forces, Source of National Pride</p>
                         <div class="printable-form__head__letterhead">
@@ -32,7 +33,146 @@
                         </div>
                     </div>
                     <!-- form content -->
+
                     <div class="printable-form__body">
+                        <span class="printable-form__body__title">Request for Quotation</span>
+                        <table class="printable-form__body__table
+                                      printable-form__body__table--layout">
+                            <tr>
+                                <td class="align-left" width="50%">{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s',$data['transaction_date'])->format('d F Y')}}</td>
+                                <td class="align-right" width="50%">RFQ No: {{$data['rfq_number']}}</td>
+                            </tr>
+                        </table>
+                        <p>SIR/MADAME,</p>
+                        <p>Please quote your lowest price on the items listed below, subject to the following conditions and submit your quotation duly signed by you or your representative not later than {{\Carbon\Carbon::createFromFormat( 'Y-m-d H:i:s', $data['deadline'])->format('dHi F y') }}</p>
+                       {{--  <table class="printable-form__body__table
+                                      printable-form__body__table--borderless">
+                            <tr>
+                                <td width="45%" height="30px"></td>
+                                <td width="10%" height="30px"></td>
+                                <td width="45%" height="30px"></td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center v-align-middle" width="45%"></td>
+                                <td width="10%"></td>
+                                <td class="signatory align-left v-align-middle" width="45%">
+                                    <div class="signatory-name">
+                                        <table>
+                                            <tr>
+                                                <td><strong>LTC THIRD TAURO PA</strong></td>
+                                            </tr>
+                                            <tr>
+                                                <td class="v-align-top" height="40px">Head</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table> --}}
+                        <ol>
+                            <li>DELIVERY PERIOD IS ATLEAST SEVEN (7) CALENDAR DAYS AT GHQ HEADQUARTERS</li>
+                            <li>WARRANTY SHALL BE FOR THE PERIOD OF THREE (3) MONTHS FOR SUPPLIES & MATERIALS, ONE (1) YEAR FOR EQUIPMENT, FROM DATE OF ACCEPTANCE BY THE PROCURING ENTITY OR PRODUCT WARRANTY WHICHEVER IS LONGER.</li>
+                            <li>PRICE VALIDITY SHALL BE FIXED DURING THE BIDDERS PERFORMANCE OF THE CONTRACT AND NOT SUBJECT TO VARIATION OR PRICE ESCALATION ON ANY ACCOUNT.</li>
+                            <li>PHILGEPS REGISTRATION SHALL BE ATTACHED UPON SUBMISSION OF THE QUOTATION.</li>
+                            <li>BIDDERS SHALL SUBMIT ORIGINAL DOCUMENTS SHOWING CERTIFICATIONS OF THE PROJECT BEING OFFERED OR ITS EQUIVALENT, IF NECESSARY.</li>
+                            <li>APPROVED BUDGET FOR THE CONTRACT IS: <strong style="text-transform:uppercase">{{translateToWords($data['total_amount'])}} PESOS ONLY. ( Php{{formatPrice($data['total_amount'])}})</strong></li>
+                        </ol>
+                        <table class="printable-form__body__table classic">
+                            <tr>
+                                <td width="10%">Item No.</td>
+                                <td width="10%">Qunatity</td>
+                                <td width="10%">UOM</td>
+                                <td width="30%">Description</td>
+                                <td width="10%">Unit Price</td>
+                                <td width="20%">Total Price</td>
+                            </tr>
+                            @foreach($data['items'] as $key => $item)
+                                <tr>
+                                    <td style="text-align:center">{{$key+1}}</td>
+                                    <td>{{$item->quantity}}</td>
+                                    <td>{{$item->unit_measurement}}</td>
+                                    <td style="text-align:left">{{$item->item_description}}</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+                            <tr>
+                                <td class="align-center" colspan="5">x-x-x-x-x Nothing Follows x-x-x-x-x</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="5" class="align-center"><strong>TOTAL PhP</strong></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                        <p>After having carefully read and accepted your General Conditions, I/We hereby certify the item/s quoted is of the quality/standard at the price/s noted above.</p>
+                        <!-- form signatories -->
+                        <table class="printable-form__body__table
+                                      printable-form__body__table--borderless">
+                            <tr>
+                                <td class="signatory align-center v-align-bottom" width="40%" height="60px"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center v-align-bottom" width="50%" height="60px"></td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center" width="40%"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center" width="50%">
+                                    <span class="conforme-label">Signature over Printed Name</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center v-align-bottom" width="40%" height="30px"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center v-align-bottom" width="50%" height="30px"></td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center" width="40%"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center" width="50%">
+                                    <span class="conforme-label">Business Name</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center v-align-bottom" width="40%" height="30px"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center v-align-bottom" width="50%" height="30px"></td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center" width="40%"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center" width="50%">
+                                    <span class="conforme-label">Business Address</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center v-align-bottom" width="40%" height="30px"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center v-align-bottom" width="50%" height="30px"></td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center" width="40%"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center" width="50%">
+                                    <span class="conforme-label">Tel No / Cell No</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center v-align-bottom" width="40%" height="30px"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center v-align-bottom" width="50%" height="30px"></td>
+                            </tr>
+                            <tr>
+                                <td class="signatory align-center" width="40%"></td>
+                                <td class="signatory align-center" width="10%"></td>
+                                <td class="signatory align-center" width="50%">
+                                    <span class="conforme-label">Date</span>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+
+                    {{-- <div class="printable-form__body">
                         <span class="printable-form__body__title">Request for Quotation</span>
                         <table class="printable-form__body__table
                                       printable-form__body__table--layout">
@@ -153,7 +293,7 @@
                             </tr>
                         </table>
                     </div>
-                </div>
+                </div> --}}
             </div>
 
     </body>
