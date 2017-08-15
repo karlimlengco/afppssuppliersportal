937fb0fd53fb09f40ee3cf8af958bc9f26069dd6
Karl Limlengco <kslimlengco@revlv.net>
2017-08-15 23:06:32 +0800

    ispq

--- resources/views/forms/ispq.blade.php
+++ resources/views/forms/ispq.blade.php
@@ -33,7 +33,75 @@
                 </div>
                 <!-- form content -->
                 <div class="printable-form__body">
+
                     <span class="printable-form__body__title">Invitation to Submit a Quotation / Proposal</span>
+                    <p>{{\Carbon\Carbon::createFromFormat('Y-m-d',$data['transaction_date'])->format('d F Y')}}</p>
+                    <p>1. The Armed Forces of the Philippines through the 302ND  Canvass/Contracting Committee invites PhilGEPS registered suppliers to submit quotation/proposal for the following list of procurement with corresponding Approved Budget for the Contract (ABC).</p>
+                    <table class="printable-form__body__table classic">
+                        <tr>
+                            <td width="10%">ITEM NO</td>
+                            <td width="10%">UPR NO</td>
+                            <td width="20%">RFQ NO</td>
+                            <td width="10%">MP</td>
+                            <td width="30%">ITEM DESCRIPTION</td>
+                            <td width="10%">ABC</td>
+                            <td width="20%">CANVASSING DATE/TIME</td>
+                        </tr>
+                        @foreach($data['quotations'] as $key => $quotation)
+                            <tr>
+                                <td style="text-align:center">{{$key+1}}</td>
+                                <td>{{$quotation->upr_number}}</td>
+                                <td>{{$quotation->rfq_number}}</td>
+                                <td>@if($quotation->upr) @if($quotation->upr->modes) {{$quotation->upr->modes->name}} @endif @endif</td>
+                                <td style="text-align:left">{{$quotation->description}}</td>
+                                <td>{{formatPrice($quotation->total_amount)}}</td>
+                                <td>{{$quotation->canvassing_date}} {{$quotation->canvassing_time}}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+
+                    <p>2. Interested Suppliers may obtain further information from Canvass Contracting Committee Secretariat at the office of 302ND Contracting Office, {{$data['venue']}} for the issuance of Request for Quotation (RFQ) from <strong>8:00 AM - 5:00 PM Monday to Friday excluding Saturdays, Sundays and Holidays</strong>.</p>
+                    <p>3. Quotations must be delivered on or before the <strong>stated canvassing date/time at GHQ HEAD OFFICE</strong> . All quotations must be accompanied by eligibility documents as required.</p>
+                    <p>4. RFQ will be opened in the presense of the Suppliers or their representative/s who choose to attend at the address stated above. Late quotations shall not be accepted.</p>
+                    <p>5. The AFP reserves the right to reject any or all proposals, or declare a failure of canvass, or not award the contract, without thereby incurring any liability in accordance with the Republic Act No. 9184 and its Revised Implementing Rules and Regulations.</p>
+                    <p>6. For inquiries, interested parties may contact the Secretariat, Canvass/Contracting Committee at telephone No.</p>
+                    <!-- form signatories -->
+                    <table class="printable-form__body__table
+                                  printable-form__body__table--borderless">
+                        <tr>
+                            <td class="v-align-bottom align-center" width="45%" height="80px"></td>
+                            <td class="v-align-bottom align-center" width="10%" height="80px"></td>
+                            <td class="v-align-bottom align-center" width="45%" height="80px"></td>
+                        </tr>
+                        <tr>
+                            <td class="signatory align-left v-align-middle" width="45%"></td>
+                            <td width="10%"></td>
+                            <td class="signatory align-center v-align-middle" width="45%">
+                                <div class="signatory-name">
+                                    <table>
+                                        <tr>
+                                            <td nowrap><strong>{{$data['signatories']->name}}</strong></td>
+                                            <td width="100%"></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="align-justify">
+                                                <div class="signatory-rank-justify">
+                                                    <strong>{{$data['signatories']->ranks}}</strong>
+                                                    <span></span>
+                                                </div>
+                                            </td>
+                                            <td width="100%"></td>
+                                        </tr>
+                                        <tr>
+                                            <td class="v-align-bottom" height="20px">{{$data['signatories']->designation}}</td>
+                                            <td width="100%" height="20px"></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                    {{-- <span class="printable-form__body__title">Invitation to Submit a Quotation / Proposal</span>
                     <p>{{\Carbon\Carbon::createFromFormat('Y-m-d',$data['transaction_date'])->format('d F Y')}}</p>
                     <p>1. The Armed Forces of the Philippines through the 302ND  Canvass/Contracting Committee invites PhilGEPS registered suppliers to submit quotation/proposal for the following list of procurement with corresponding Approved Budget for the Contract (ABC).</p>
                     <table class="printable-form__body__table">
@@ -101,7 +169,7 @@
                                 {{$data['signatories']->designation}}<br>
                             </td>
                         </tr>
-                    </table>
+                    </table> --}}
                 </div>
             </div>
 
