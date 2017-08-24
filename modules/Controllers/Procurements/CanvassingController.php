b37ae224f6916e0bbd3bc4e7952198ed98b142dd
Karl Limlengco <kslimlengco@revlv.net>
2017-08-24 17:22:47 +0800

    update

--- modules/Controllers/Procurements/CanvassingController.php
+++ modules/Controllers/Procurements/CanvassingController.php
@@ -388,24 +388,24 @@ class CanvassingController extends Controller
 
         $holiday_lists          =   $holidays->lists('id','holiday_date');
 
-        $ispq_transaction_date  =   $rfq_model->completed_at;
+        // $ispq_transaction_date  =   $rfq_model->completed_at;
 
-        $day_delayed            =   $ispq_transaction_date->diffInDaysFiltered(function(Carbon $date)use ($holiday_lists) {
-            return $date->isWeekday() && !in_array($date->format('Y-m-d'), $holiday_lists);
-        }, $transaction_date);
+        // $day_delayed            =   $ispq_transaction_date->diffInDaysFiltered(function(Carbon $date)use ($holiday_lists) {
+        //     return $date->isWeekday() && !in_array($date->format('Y-m-d'), $holiday_lists);
+        // }, $transaction_date);
 
-        $cd                     =   $ispq_transaction_date->diffInDays($transaction_date);
-        $wd                     =   ($day_delayed > 0) ?  $day_delayed - 1 : 0;
+        // $cd                     =   $ispq_transaction_date->diffInDays($transaction_date);
+        // $wd                     =   ($day_delayed > 0) ?  $day_delayed - 1 : 0;
 
-        if($day_delayed != 0)
-        {
-            $day_delayed            =   $day_delayed - 1;
-        }
+        // if($day_delayed != 0)
+        // {
+        //     $day_delayed            =   $day_delayed - 1;
+        // }
 
-        if($wd != $result->days)
-        {
-            $model->update(['days' => $wd], $id);
-        }
+        // if($wd != $result->days)
+        // {
+            // $model->update(['days' => $wd], $id);
+        // }
 
         $modelType  =   'Revlv\Procurements\Canvassing\CanvassingEloquent';
         $resultLog  =   $audits->findLastByModelAndId($modelType, $id);
