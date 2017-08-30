51e197e8012659df2629d089c6f3c22846ef0895
Karl Limlengco <kslimlengco@revlv.net>
2017-08-30 17:04:52 +0800

    add new user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $faker = Faker::create();
 
         $password = Hash::make("password");
+        $password2 = Hash::make("12345");
 
         \App\User::create([
             "email"         => 'kblimlengco@yahoo.com',
@@ -48,6 +49,49 @@ class UsersTableSeeder extends Seeder
             "contact_number"=> '09363494228'
         ]);
 
+        \App\User::create([
+            "email"         => 'thirdtauro@yahoo.com',
+            "password"      => $password2,
+            "username"      => 'thirdtauro',
+            "first_name"    => 'Romeo',
+            "surname"       => 'Tauro',
+            "middle_name"   => 'Cocjin',
+            "gender"        => 'male',
+            "address"       => 'ACS for CEIS, OPS6, AFPPS CGEA, QC',
+            "status"        => '1',
+            "designation"   => 'Network Admin',
+            "contact_number"=> '09162630925'
+        ]);
+
+        \App\User::create([
+            "email"         => 'commander@afpps.ph',
+            "password"      => $password2,
+            "username"      => 'C,AFPPS',
+            "first_name"    => 'Ernesto',
+            "surname"       => 'T',
+            "middle_name"   => 'Lopena',
+            "gender"        => 'male',
+            "address"       => 'Camp Aguinaldo',
+            "status"        => '1',
+            "designation"   => 'Commander, AFPPS',
+            "contact_number"=> '5738492'
+        ]);
+
+        \App\User::create([
+            "email"         => 'spade133876@yahoo.com',
+            "password"      => $password2,
+            "username"      => 'CHIEF, OPS6',
+            "first_name"    => 'FRANCISCO BUTCH',
+            "surname"       => 'B',
+            "middle_name"   => 'CANZADO',
+            "gender"        => 'male',
+            "address"       => 'HAFP Procurement Service, Camp General
+Emilio Aguinaldo, Quezon City',
+            "status"        => '1',
+            "designation"   => 'Chief OPS6',
+            "contact_number"=> '09399395771'
+        ]);
+
 
         \App\Activation::create([
             "user_id"         => 1,
@@ -58,6 +102,24 @@ class UsersTableSeeder extends Seeder
         \App\Activation::create([
             "user_id"         => 2,
             "code"         => 2,
+            "completed"         => 1,
+        ]);
+
+        \App\Activation::create([
+            "user_id"         => 3,
+            "code"         => 3,
+            "completed"         => 1,
+        ]);
+
+        \App\Activation::create([
+            "user_id"         => 4,
+            "code"         => 4,
+            "completed"         => 1,
+        ]);
+
+        \App\Activation::create([
+            "user_id"         => 5,
+            "code"         => 5,
             "completed"         => 1,
         ]);
     }
