43f542d56bff38835273e07a69800b7dc48017b2
Karl Limlengco <kslimlengco@revlv.net>
2017-08-30 14:20:13 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -16649,11 +16649,12 @@ var tarray2IDs = [];
             }
         },
         clickItemUnit: function clickItemUnit(item) {
-            if (this.show == false) {
-                this.show = true;
-            } else {
-                this.show = false;
-            }
+            // if(this.show == false){
+            this.show = true;
+            // }else{
+            //     this.show = false;
+            // }
+
 
             if (arrayProgramCenter.indexOf(item.name) == -1 && arrayProgramCenter[item.name] == item.name) {
                 if (array2IDs.indexOf(item.short_code) == -1) {
