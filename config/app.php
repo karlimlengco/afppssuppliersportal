938c9cf39779d790943fc863ddb590d989f73675
Karl Limlengco <kslimlengco@revlv.net>
2017-10-15 23:10:33 +0800

    update

--- config/app.php
+++ config/app.php
@@ -178,7 +178,6 @@ return [
         Barryvdh\Snappy\ServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
-        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
 
         /*
          * Revlv
@@ -260,7 +259,6 @@ return [
         'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
         'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
         'Uuid' => Webpatser\Uuid\Uuid::class,
-        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
     ],
 
 ];
