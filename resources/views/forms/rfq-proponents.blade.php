e3a8438d77400cf3fdc21340506dc41468a38c2a
Karl Limlengco <kslimlengco@revlv.net>
2017-11-16 09:28:08 +0800

    added page break

--- resources/views/forms/rfq-proponents.blade.php
+++ resources/views/forms/rfq-proponents.blade.php
@@ -89,7 +89,7 @@
                         </table>
                         <p>After having carefully read and accepted your General Conditions, I/We hereby certify the item/s quoted is of the quality/standard at the price/s noted above.</p>
                         <!-- form signatories -->
-                        <table class="printable-form__body__table no-border no-padding" style="page-break-inside: avoid;">
+                        <table class="printable-form__body__table no-border no-padding" style="page-break-before: always;">
                             <tr>
                                 <td width="45%"></td>
                                 <td width="10%"></td>
