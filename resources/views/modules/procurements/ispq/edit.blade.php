abdd6fcf8872eb3836790eb8de1c847eb5ae1d3c
Karl Limlengco <kslimlengco@revlv.net>
2018-02-15 11:42:30 +0800

    updateg

--- resources/views/modules/procurements/ispq/edit.blade.php
+++ resources/views/modules/procurements/ispq/edit.blade.php
@@ -94,7 +94,7 @@ Invitation to Submit Price Quotation
                     @foreach($data->quotations as $quotation)
                     <tr>
                         <td><a href="{{route('procurements.unit-purchase-requests.show',$quotation->upr_id)}}">{{$quotation->upr_number}}</a></td>
-                        <td>{{$quotation->rfq_number}}</td>
+                        <td>{{str_replace('AMP', 'RFQ', $quotation->rfq_number) }}</td>
                         <td>{{$quotation->description}}</td>
                         <td>{{$quotation->total_amount}}</td>
                         <td>{{$quotation->canvassing_date}}</td>
