d6a4c4884db50d8b804ddffd321af22ebed9846f
Karl Limlengco <kslimlengco@revlv.net>
2018-03-06 11:43:44 +0800

    update

--- resources/macros/macros.php
+++ resources/macros/macros.php
@@ -589,7 +589,7 @@ function translateToWords($number)
 {
 
   $num = str_replace(',', '', $number);
-  $num = number_format($number,2);
+  $num = number_format($num,2);
   $num = str_replace(',', '', $num);
   $split = explode('.',$num);
   $cents = '';
