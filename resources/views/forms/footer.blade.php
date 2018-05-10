c3c04fb5cd03e31238cea0181b448a02ceedccfe
Karl Limlengco <kslimlengco@revlv.net>
2018-05-11 06:38:46 +0800

    test

--- resources/views/forms/footer.blade.php
+++ resources/views/forms/footer.blade.php
@@ -3,16 +3,16 @@
 <head>
 </head>
 <body>
-<div class="printable-form__foot" style="font-size: 12px; font-weight: 400; color: #545454;">
-    <table class="printable-form__foot__table" style="width: 100%; border-collapse: collapse;">
+<div class="printable-form__foot" >
+    <table class="printable-form__foot__table" >
         <tr>
-            <td colspan="2" style="width: 50%; padding: 0 15px;">
-                <p class="printable-form__foot__values" style="display: block;  width: 100%; text-align: center; padding: 0; margin: 0 0 20px 0;">AFP Core Values: Honor, Service, Patriotism</p>
+            <td colspan="2" >
+                <p class="printable-form__foot__values">AFP Core Values: Honor, Service, Patriotism</p>
             </td>
         </tr>
         <tr>
-            <td style="width: 50%; padding: 0 15px;">
-                <span class="printable-form__foot__ref" style="display: block; width: 100%; font-size: 8px; font-weight: 800; text-transform: uppercase; text-align: left;"></span>
+            <td>
+                <span class="printable-form__foot__ref"></span>
             </td>
         </tr>
     </table>
