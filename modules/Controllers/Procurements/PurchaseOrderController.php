a28a8269c77f13ac48283d8c5991e46e004ce3a0
Karl <kslimlengco@revlv.net>
2018-06-19 09:08:16 +0800

    update

--- modules/Controllers/Procurements/PurchaseOrderController.php
+++ modules/Controllers/Procurements/PurchaseOrderController.php
@@ -1209,7 +1209,7 @@ class PurchaseOrderController extends Controller
 
         $pdf = PDF::loadView('forms.po', ['data' => $data])
             ->setOption('margin-bottom', 30)
-            ->setOption('margin-top', 30);
+            ->setOption('margin-top', 35);
             // ->setOption('footer-html', route('pdf.footer'));
 
         return $pdf->setOption('page-width', '8.5in')->setOption('page-height', '14in')->inline('po.pdf');
@@ -1274,7 +1274,7 @@ class PurchaseOrderController extends Controller
 
         $pdf = PDF::loadView('forms.po3', ['data' => $data])
             ->setOption('margin-bottom', 30)
-            ->setOption('margin-top', 30);
+            ->setOption('margin-top', 35);
             // ->setOption('footer-html', route('pdf.footer'));
 
         return $pdf->setOption('page-width', '8.5in')->setOption('page-height', '14in')->inline('po.pdf');
@@ -1453,7 +1453,7 @@ class PurchaseOrderController extends Controller
 
         $pdf = PDF::loadView('forms.po-form2', ['content' => $output, 'data' => $data])
             ->setOption('margin-bottom', 30)
-            ->setOption('margin-top', 30);
+            ->setOption('margin-top', 35);
             // ->setOption('footer-html', route('pdf.footer'));
         return $pdf->setOption('page-width', '8.5in')->setOption('page-height', '14in')->inline('voucher.pdf');
 
@@ -1504,7 +1504,7 @@ class PurchaseOrderController extends Controller
 
         $pdf = PDF::loadView('forms.po-coa', ['data' => $data])
             ->setOption('margin-bottom', 30)
-            ->setOption('margin-top', 30);
+            ->setOption('margin-top', 35);
             // ->setOption('footer-html', route('pdf.footer'));
 
         return $pdf->setOption('page-width', '8.5in')->setOption('page-height', '14in')->inline('po-coa.pdf');
