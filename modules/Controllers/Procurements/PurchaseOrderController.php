f761ac9352bbe8cbbd77ad4504990ff0af0e8c65
Karl <kslimlengco@revlv.net>
2018-07-05 07:32:16 +0800

    contract pages

--- modules/Controllers/Procurements/PurchaseOrderController.php
+++ modules/Controllers/Procurements/PurchaseOrderController.php
@@ -1141,7 +1141,7 @@ class PurchaseOrderController extends Controller
                 $totalPages = $totalPages + 1;
             }
         }
-        $totalPages = ceil($totalPages/17) + 2;
+        $totalPages = ceil($totalPages/11) + 2;
         $pdf = PDF::loadView('forms.po-contract', ['data' => $data, 'totalPages' => $totalPages])
             ->setOption('margin-bottom', 30);
             // ->setOption('footer-html', route('pdf.footer'));
