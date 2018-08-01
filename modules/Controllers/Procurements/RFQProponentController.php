b38f6cac4cc07b8dfb59bf769de37adc7b3a568d
Karl <kslimlengco@revlv.net>
2018-08-02 07:06:05 +0800

    fix

--- modules/Controllers/Procurements/RFQProponentController.php
+++ modules/Controllers/Procurements/RFQProponentController.php
@@ -337,7 +337,8 @@ class RFQProponentController extends Controller
         }
         else
         {
-            $data['deadline']           = \Carbon\Carbon::createFromFormat( 'Y-m-d H:i:s', $rfq->upr->invitations->canvassing_date ." ". $rfq->upr->invitations->canvassing_time);
+            $data['deadline']           = $rfq->deadline;
+            // $data['deadline']           = \Carbon\Carbon::createFromFormat( 'Y-m-d H:i:s', $rfq->upr->invitations->canvassing_date ." ". $rfq->upr->invitations->canvassing_time);
         }
 
 
