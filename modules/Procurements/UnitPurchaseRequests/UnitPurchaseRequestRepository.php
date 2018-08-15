2af9a929f4df3da54864ed0cfd1d658ea6758887
Karl <kslimlengco@revlv.net>
2018-08-15 14:40:58 +0800

    fix

--- modules/Procurements/UnitPurchaseRequests/UnitPurchaseRequestRepository.php
+++ modules/Procurements/UnitPurchaseRequests/UnitPurchaseRequestRepository.php
@@ -580,7 +580,7 @@ class UnitPurchaseRequestRepository extends BaseRepository
             $model  =   $model->where('unit_purchase_requests.status', '<>', "cancelled");
             $model  =   $model->whereRaw("unit_purchase_requests.next_due <  NOW() ");
         }
-
+        dd($pcco);
         if($pcco != null)
         {
             $model  =   $model->where('procurement_centers.name', '=', $pcco);
