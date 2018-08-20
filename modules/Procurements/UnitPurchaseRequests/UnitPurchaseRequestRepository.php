890fad45851f417957b7515aababc2c8f805846b
Karl <kslimlengco@revlv.net>
2018-08-20 09:00:01 +0800

    fix

--- modules/Procurements/UnitPurchaseRequests/UnitPurchaseRequestRepository.php
+++ modules/Procurements/UnitPurchaseRequests/UnitPurchaseRequestRepository.php
@@ -552,7 +552,7 @@ class UnitPurchaseRequestRepository extends BaseRepository
         $model  =   $model->leftJoin('procurement_centers', 'procurement_centers.id', '=', 'unit_purchase_requests.procurement_office');
         // $model  =   $model->where('procurement_centers.name', '=', $name);
         // $model  =   $model->where('catered_units.short_code', '=', $programs);
-        $model  =   $model->where('procurement_centers.programs', '=', $programs);
+        // $model  =   $model->where('procurement_centers.programs', '=', $programs);
 
         $model  =   $model->where('unit_purchase_requests.status', '!=', 'draft');
 
@@ -580,15 +580,15 @@ class UnitPurchaseRequestRepository extends BaseRepository
             $model  =   $model->whereRaw("unit_purchase_requests.next_due <  NOW() ");
         }
 
-        // if($pcco != null)
-        // {
-        //     $model  =   $model->where('procurement_centers.name', '=', $pcco);
-        // }
+        if($pcco != null)
+        {
+            $model  =   $model->where('procurement_centers.name', '=', $pcco);
+        }
 
-        // if($unit != null)
-        // {
-        //     $model  =   $model->where('catered_units.short_code', '=', $unit);
-        // }
+        if($unit != null)
+        {
+            $model  =   $model->where('catered_units.short_code', '=', $unit);
+        }
 
         if($type != 'alternative')
         {
