1734c9f59a8532c7c7b4ee1d773308ff9d779755
Karl <kslimlengco@revlv.net>
2018-10-05 02:18:34 +0800

    fix

--- modules/Procurements/UnitPurchaseRequests/DatatableTrait.php
+++ modules/Procurements/UnitPurchaseRequests/DatatableTrait.php
@@ -123,30 +123,14 @@ trait DatatableTrait
             // 'mode_of_procurements.name as type',
             DB::raw("CONCAT(users.first_name,' ', users.surname) AS full_name"),
             DB::raw("CASE WHEN mode_of_procurements.name IS NULL THEN 'Public Bidding' ELSE mode_of_procurements.name END as type"),
-            DB::raw("COUNT(unit_purchase_request_items.id) as item_count"),
+            // DB::raw("COUNT(unit_purchase_request_items.id) as item_count"),
             // DB::raw("datediff(NOW(), unit_purchase_requests.date_prepared ) as calendar_days")
         ]);
 
         $model  =   $model->leftJoin('users', 'users.id', '=', 'unit_purchase_requests.prepared_by');
         $model  =   $model->leftJoin('mode_of_procurements', 'mode_of_procurements.id', '=', 'unit_purchase_requests.mode_of_procurement');
-        $model  =   $model->leftJoin('unit_purchase_request_items', 'unit_purchase_request_items.upr_id', '=', 'unit_purchase_requests.id');
-
-        $model  =   $model->groupBy([
-            'unit_purchase_requests.id',
-            'unit_purchase_requests.upr_number',
-            'unit_purchase_requests.date_prepared',
-            'unit_purchase_requests.date_processed',
-            'unit_purchase_requests.ref_number',
-            'unit_purchase_requests.total_amount',
-            'unit_purchase_requests.project_name',
-            'unit_purchase_requests.prepared_by',
-            'unit_purchase_requests.status',
-            'unit_purchase_requests.state',
-            'unit_purchase_requests.created_at',
-            'users.first_name',
-            'users.surname',
-            'mode_of_procurements.name',
-        ]);
+        // $model  =   $model->leftJoin('unit_purchase_request_items', 'unit_purchase_request_items.upr_id', '=', 'unit_purchase_requests.id');
+ 
 
         $model  =   $model->orderBy('created_at', 'desc');
 
